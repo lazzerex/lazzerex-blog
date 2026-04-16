153a8e2defb7f99d348b03e448aae06608fe4a2a
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-04-16 23:09:39 +0700

    feat: config env

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
 	readonly NOTION_TOKEN?: string;
 	readonly NOTION_API_KEY?: string;
 	readonly NOTION_DATABASE_ID?: string;
+	readonly PUBLIC_GO_API_BASE_URL?: string;
 }
 
 interface ImportMeta {
