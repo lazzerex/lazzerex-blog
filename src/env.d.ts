9bde6fae0bb4a0aab4ae720e787037d7c790c428
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-04-30 15:48:48 +0700

    chore: update environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
 	readonly NOTION_TOKEN?: string;
 	readonly NOTION_API_KEY?: string;
 	readonly NOTION_DATABASE_ID?: string;
+	readonly NOTION_PUBLIC_SITE_URL?: string;
 	readonly PUBLIC_GO_API_BASE_URL?: string;
 	readonly GO_API_PUBLISH_SYNC_SECRET?: string;
 }
