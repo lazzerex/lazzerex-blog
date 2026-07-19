5c10ba2bde8d97ff03135cf312defba473c357e5
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-07-19 15:00:12 +0700

    chore: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: "static",
   site: "https://lazzerex-blog.vercel.app",
   prefetch: {
     defaultStrategy: "hover"
-  }
+  },
+  integrations: [sitemap()]
 });
